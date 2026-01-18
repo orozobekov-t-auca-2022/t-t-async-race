ee728fd38788d2a0d30eb1dd5a882809686bcbe4
turynkhan <tursynkhan001@gmail.com>
2026-01-18 21:34:52 +0500

    init: setup project

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import unicorn from "eslint-plugin-unicorn";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: { unicorn },
+    rules: {
+      ...unicorn.configs.recommended.rules,
+
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "unicorn/no-array-callback-reference": "off",
+      "unicorn/no-array-for-each": "off",
+      "unicorn/no-array-reduce": "off",
+      "unicorn/no-null": "off",
+      "unicorn/number-literal-case": "off",
+      "unicorn/numeric-separators-style": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: {
+            acc: true,
+            env: true,
+            i: true,
+            j: true,
+            props: true,
+            Props: true,
+          },
+        },
+      ],
+    },
+  },
+];
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import './styles.css';
+import { App } from './app/app';
+
+const app = new App();
