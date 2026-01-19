532cff5c714d916773aba9bb1d0cd029ee85ba62
turynkhan <tursynkhan001@gmail.com>
2026-01-19 12:57:42 +0500

    feat: implement basic structure for App class

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,17 @@
-export class App {}
+export class App {
+  private readonly root: HTMLDivElement;
+  private readonly container: HTMLDivElement = document.createElement('div');
+  private readonly nav: HTMLDivElement = document.createElement('div');
+
+  public constructor(root: HTMLDivElement) {
+    this.root = root;
+  }
+
+  public mount(): void {
+    this.container.className = 'container';
+    this.nav.className = 'nav';
+    this.container.append(this.nav);
+
+    this.root.append(this.container);
+  }
+}
