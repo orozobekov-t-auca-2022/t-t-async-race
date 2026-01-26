d80ddf026422115c7e2b2f0f05b6fbb6baf9c4c9
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2026-01-27 01:35:41 +0600

    fix: fix edit form bug

--- src/app/app.ts
+++ src/app/app.ts
@@ -89,8 +89,6 @@ export class App {
       const carId =
         carElement instanceof HTMLElement && carElement.dataset.carId ? Number(carElement.dataset.carId) : null;
 
-      const formGroup = button.closest('.form-group');
-
       switch (action) {
         case 'select': {
           if (carId) void this.handleSelectCar(carId);
@@ -105,11 +103,13 @@ export class App {
           break;
         }
         case 'create': {
-          if (formGroup instanceof HTMLElement) void this.handleCreateCar(formGroup);
+          const createFieldset = button.closest('fieldset[data-form="create"]');
+          if (createFieldset instanceof HTMLElement) void this.handleCreateCar(createFieldset);
           break;
         }
         case 'edit': {
-          if (formGroup instanceof HTMLElement) void this.handleEditCar(formGroup);
+          const editFieldset = button.closest('fieldset[data-form="edit"]');
+          if (editFieldset instanceof HTMLElement) void this.handleEditCar(editFieldset);
           break;
         }
         case 'prev': {
@@ -149,18 +149,15 @@ export class App {
     const car = cars.find((c) => c.id === carId);
 
     if (!car) return;
-
     this.selectedCarId = carId;
 
-    const editFieldset = this.content.querySelector('fieldset:nth-of-type(2)');
-    if (!(editFieldset instanceof HTMLFieldSetElement)) return;
-    editFieldset.disabled = false;
+    const editFieldset = this.content.querySelector('fieldset[data-form="edit"]');
+    if (editFieldset instanceof HTMLFieldSetElement) {
+      editFieldset.disabled = false;
+    }
 
-    const editForm = this.content.querySelector('fieldset:nth-of-type(2) .form-group');
-    if (!(editForm instanceof HTMLElement)) return;
-
-    const nameInput = editForm?.querySelector('input[type="text"]');
-    const colorInput = editForm?.querySelector('input[type="color"]');
+    const nameInput = editFieldset?.querySelector('input[type="text"]');
+    const colorInput = editFieldset?.querySelector('input[type="color"]');
 
     if (nameInput instanceof HTMLInputElement && colorInput instanceof HTMLInputElement) {
       nameInput.value = car.name;
@@ -230,11 +227,13 @@ export class App {
       this.selectedCarId = null;
       nameInput.value = '';
       colorInput.value = '#000000';
+     
+      const editFieldset = this.content.querySelector('fieldset[data-form="edit"]');
+      if (editFieldset instanceof HTMLFieldSetElement) {
+        editFieldset.disabled = true;
+      }
+     
       await this.renderGarage();
-
-      const editFieldset = this.content.querySelector('fieldset:nth-of-type(2)');
-      if (!(editFieldset instanceof HTMLFieldSetElement)) return;
-      editFieldset.disabled = true;
     } catch (error) {
       console.error('Failed to update car:', error);
     }
